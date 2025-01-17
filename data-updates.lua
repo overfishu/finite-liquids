773ad333ca235ebff2c376ae73b81a196b833a67
Guillaume MRX <overfishu@users.noreply.github.com>
2025-01-17 22:18:11 +0300

    Create data-updates.lua

---
+++ data-updates.lua
@@ -0,0 +1,14 @@
+data.raw.resource["crude-oil"].infinite = false -- true
+data.raw.resource["crude-oil"].minable =
+{
+    mining_time = 0.1, -- 1
+    results =
+    {
+        {
+            type = "fluid",
+            name = "crude-oil",
+            amount = 1, -- 10
+            probability = 1 -- 10
+        }
+    }
+}
